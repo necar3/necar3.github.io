edfe20ee8ef36be0bfd83fed1efb448c77fed55f
olamarche <olamarche@gmail.com>
2015-08-14 21:28:39 -0400

    Add test image and remove header on projets.html

---
+++ fastshell/src/js/scripts.js
@@ -0,0 +1,13 @@
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    $(document).ready(function() {
+			$('#fullpage').fullpage({
+				slidesNavigation: true
+			});
+		});
+  });
+
+})(jQuery, window, document);
