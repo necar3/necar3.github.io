41e14887ebf13cdcdafc51e5ef2faea0a8fd1626
Olivier Lamarche <olamarche@gmail.com>
2015-12-02 21:36:53 -0500

    fix navigation

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -1,7 +1,8 @@
 $(function () {
   $(document).ready(function() {
     $('#fullpage').fullpage({
-      slidesNavigation: true
+      slidesNavigation: true,
+      anchors:['projet1', 'projet2', 'projet3', 'projet4', 'projet5', 'projet6', 'projet7', 'projet8', 'projet9', 'projet10']
     });
   });
 });
